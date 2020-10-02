236435b067fe40c79a124c4f88f855c26a3a6fba
Thomas-Le <elsamoht@live.unc.edu>
2020-10-02 13:40:08 -0400

    Add migration for avian diet table

---
+++ src/migrations/1601660339357-AvianDietTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AvianDietTable1601660339357 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
