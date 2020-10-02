2a91f1b5cbb178d6b1a8fcdd6ac93e67a18b7034
Thomas-Le <elsamoht@live.unc.edu>
2020-10-02 13:33:16 -0400

    Add ApolloServer and TypeOrm in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
 import 'reflect-metadata'
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { createConnection } from "typeorm";
+import { buildSchema } from "type-graphql";
+import { AvianDietResolver } from './resolvers/AvianDietResolver';
 
-console.log('hello world')
+const main = async () => {
+    const app = express();
+
+    const conn = await createConnection();
+    conn.runMigrations();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [AvianDietResolver],
+        }),
+        context: ({ req, res }) => ({ req, res })
+    });
+
+    apolloServer.applyMiddleware({ app, cors: false });
+
+    app.listen(4000, () => {
+        console.log('Server running on port 4000');
+    });
+};
+
+main();
