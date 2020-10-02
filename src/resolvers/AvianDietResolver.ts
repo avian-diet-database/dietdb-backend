bfe80c3a0605a8c9113a52be1226ffd0b63959e6
Thomas-Le <elsamoht@live.unc.edu>
2020-10-02 13:56:22 -0400

    Add getPreyOf query

--- src/resolvers/AvianDietResolver.ts
+++ src/resolvers/AvianDietResolver.ts
@@ -1,9 +1,72 @@
-import {  Query, Resolver } from "type-graphql";
+import { AvianDiet } from "../entities/AvianDiet";
+import {  Args, ArgsType, Field, Int, ObjectType, Query, Resolver } from "type-graphql";
+import { createQueryBuilder } from "typeorm";
+
+@ArgsType()
+class GetPredatorOfArgs {
+    @Field()
+    predatorName: string;
+
+    @Field({ defaultValue: "prey_order"})
+    preyLevel: string;
+
+    @Field({ nullable: true })
+    dietType?: string;
+
+    @Field(type => Int, { nullable: true })
+    startYear?: number;
+
+    @Field(type => Int, { nullable: true })
+    endYear?: number;
+
+    @Field({ nullable: true })
+    season?: string;
+
+    @Field({ nullable: true })
+    region?: string;
+}
+
+@ObjectType()
+export class Prey {
+    @Field()
+    taxon: string;
+
+    @Field({ nullable: true })
+    items?: string;
+
+    @Field({ nullable: true })
+    wt_or_vol?: string;
+
+    @Field({ nullable: true })
+    occurence?: string;
+
+    @Field({ nullable: true })
+    unspecified?: string;
+}
 
 @Resolver()
 export class AvianDietResolver {
-    @Query(() => String)
-    async hello() {
-        return "hello world!";
+    @Query(() => [Prey])
+    async getPreyOf(@Args() {predatorName, preyLevel, dietType, startYear, endYear, season, region}: GetPredatorOfArgs) {
+        return await createQueryBuilder()
+        .select("diet." + preyLevel + " as taxon"
+        + (!dietType || dietType === "items" ? ", SUM(diet.Items) as items" : "")
+        + (!dietType || dietType === "occurence" ? ", SUM(diet.Occurrence) as occurence" : "")
+        + (!dietType || dietType === "wt_or_vol" ? ", SUM(diet.Wt_or_Vol) as wt_or_vol" : "")
+        + (!dietType || dietType === "unspecified" ? ", SUM(diet.Unspecified) as unspecified" : ""))
+        .from(subQuery => {
+            return subQuery
+                .select("*, IF(diet_type = 'Items', SUM(fraction_diet), null) as 'Items', IF(diet_type = 'Occurrence', SUM(fraction_diet), null) as 'Occurrence', IF(diet_type = 'Wt_or_Vol', SUM(fraction_diet), null) as 'Wt_or_Vol', if(diet_type = 'Unspecified', SUM(fraction_diet), null) as 'Unspecified'")
+                .from(AvianDiet, "diet")
+                .where("(common_name = :name OR scientific_name = :name) AND :level != ''"
+                + (startYear ? " AND observation_year_begin >= :start" : "")
+                + (endYear ? " AND observation_year_end <= :end" : "")
+                + (season ? " AND observation_season = :season" : "")
+                + (region ? " AND location_region = :region" : "")
+                , { name: predatorName, level: preyLevel, start: startYear, end: endYear, season: season, region: region })
+                .groupBy(preyLevel + ", source, observation_year_begin, observation_month_begin, observation_season, bird_sample_size, habitat_type, location_region, item_sample_size, diet_type")
+        }, "diet")
+        .groupBy("diet." + preyLevel + ", diet.source, diet.observation_year_begin, diet.observation_month_begin, diet.observation_season, diet.bird_sample_size, diet.habitat_type, diet.location_region, diet.item_sample_size")
+        .getRawMany();
     }
 }
