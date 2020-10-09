73665f1c83959d86341229ad25f6bc0d59d0fc7b
Thomas-Le <elsamoht@live.unc.edu>
2020-10-09 16:20:29 -0400

    Clean up getPreyOf SQL query

--- src/resolvers/AvianDietResolver.ts
+++ src/resolvers/AvianDietResolver.ts
@@ -7,7 +7,7 @@ class GetPredatorOfArgs {
     @Field()
     predatorName: string;
 
-    @Field({ defaultValue: "prey_order"})
+    @Field({ defaultValue: "order"})
     preyLevel: string;
 
     @Field({ nullable: true })
@@ -51,7 +51,7 @@ export class AvianDietResolver {
     async getPreyOf(@Args() {predatorName, preyLevel, dietType, startYear, endYear, season, region}: GetPredatorOfArgs) {
         const query = `
         SELECT taxon${!dietType || dietType == "items" ? ", SUM(Items) as items" : "" }${!dietType || dietType == "wt_or_vol" ? ", SUM(Wt_or_Vol) as wt_or_vol" : "" }${!dietType || dietType == "occurrence" ? ", SUM(Occurrence) as occurrence" : "" }${!dietType || dietType == "unspecified" ? ", SUM(Unspecified) as unspecified" : "" } FROM
-            (SELECT taxon, final1.diet_type, ROUND(SUM(Items) / n, 4) as Items, ROUND(SUM(Wt_or_Vol) / n, 4) as Wt_or_Vol, ROUND(SUM(Occurrence) / n, 4) as Occurrence, ROUND(SUM(Unspecified) / n, 4) as Unspecified FROM
+            (SELECT taxon, final1.diet_type, ROUND(SUM(Items) * 100.0 / n, 3) as Items, ROUND(SUM(Wt_or_Vol) * 100.0 / n, 3) as Wt_or_Vol, ROUND(SUM(Occurrence) * 100.0 / n, 3) as Occurrence, ROUND(SUM(Unspecified) * 100.0 / n, 3) as Unspecified FROM
 	            (SELECT diet_type, IF(prey_stage IS NOT NULL AND prey_stage != "adult", CONCAT(taxonUnid, ' ', prey_stage), taxonUnid) AS taxon, SUM(Items) as Items, SUM(Wt_or_Vol) as Wt_or_Vol, MAX(Occurrence) as Occurrence, SUM(Unspecified) as Unspecified FROM
 		            (SELECT
 			            source,
@@ -64,7 +64,7 @@ export class AvianDietResolver {
 			            item_sample_size,
 			            diet_type,
 			            prey_stage,
-			            ${Utils.getUnidTaxon(preyLevel)} AS taxonUnid,
+			            ${Utils.getUnidTaxon("prey_" + preyLevel)} AS taxonUnid,
 			            IF(diet_type = "Items", fraction_diet, NULL) as Items,
 			            IF(diet_type = "Wt_or_Vol", fraction_diet, NULL) as Wt_or_Vol,
 			            IF(diet_type = "Occurrence", fraction_diet, NULL) as Occurrence,
@@ -74,11 +74,11 @@ export class AvianDietResolver {
 		        GROUP BY source, observation_year_begin, observation_month_begin, observation_season, bird_sample_size, habitat_type, location_region, item_sample_size, taxonUnid, diet_type) final1,
 		    (SELECT diet_type, COUNT(*) AS n
 		FROM
-			(SELECT DISTINCT source, observation_year_begin, observation_month_begin, observation_season, bird_sample_size, habitat_type, location_region, location_specific, item_sample_size, diet_type, study_type
+			(SELECT DISTINCT *
 				FROM
-					(SELECT *, IF(prey_stage IS NOT NULL AND prey_stage != "adult", CONCAT(taxonUnid, ' ', prey_stage), taxonUnid) AS taxon
+					(SELECT *
                     FROM
-						(SELECT *, ${Utils.getUnidTaxon(preyLevel)} AS taxonUnid
+						(SELECT source, observation_year_begin, observation_month_begin, observation_season, bird_sample_size, habitat_type, location_region, location_specific, item_sample_size, diet_type, study_type
                         FROM avian_diet
                         WHERE (common_name = "${predatorName}" OR scientific_name = "${predatorName}")${startYear !== undefined ? " AND observation_start_year <= \"" + startYear + "\"" : ""}${endYear !== undefined ? " AND observation_end_year >= \"" + endYear + "\"": ""}${season !== undefined ? " AND observation_season = \"" + season + "\"": ""}${region !== undefined ? " AND location_region = \"" + region + "\"": ""}) AS dietspUnid) AS dietsp) AS distinctCombo GROUP BY diet_type) analysesPerDietType
                 WHERE analysesPerDietType.diet_type = final1.diet_type
